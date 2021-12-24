bfb3451d098634c01d58831068bcc925c8dda75f
fshabir <iamfurhan@gmail.com>
2021-12-24 23:35:12 +0900

    Max sublist with nested loops and O(n^2)

--- edv/ds/lists/11_max_sublist.py
+++ edv/ds/lists/11_max_sublist.py
@@ -18,11 +18,28 @@ class MaxSubList(object):
 
         return global_max
 
+    def solution2(self, lst):
+        if len(lst) == 0: return
+        if len(lst) == 1: return lst[0]
+        max_sum = lst[0]
+
+        for i in range(len(lst)):
+            current_sum = lst[i]
+            for j in range(i+1, len(lst)):
+                current_sum += lst[j]
+                max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
 
 class TestMaxSubList(unittest.TestCase):
     def test_solution1(self):
         inst = MaxSubList()
         self.assertEqual(inst.solution1([-4, 2, -5, 1, 2, 3, 6, -5, 1]), 12)
+
+    def test_solution2(self):
+        inst = MaxSubList()
+        self.assertEqual(inst.solution2([-4, 2, -5, 1, 2, 3, 6, -5, 1]), 12)
 
 
 if __name__ == "__main__":
